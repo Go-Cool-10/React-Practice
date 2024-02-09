dfc918b99900d2a1221e7948f2f0433833032608
Go-Cool-10 <gogulakrishnanparanthaman@gmail.com>
2024-02-09 12:24:17 +0530

    "first commit"

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import {useState} from 'react'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let [todos, setTodos] = useState([])
-  let[todoInput, setTodoInput] = useState('')
+  let [todos, setTodos] = useState([]);
+  let [todoInput, setTodoInput] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    todos.push(todoInput);
+    setTodos(todos);
+    console.log(todos);
+  };
   return (
     <div className="App">
-
+      <form onSubmit={handleSubmit} action="">
+        <label htmlFor="task">Enter the task</label>
+        <input
+          type="text"
+          id="task"
+          value={todoInput}
+          onChange={(e) => setTodoInput(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
